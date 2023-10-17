f159ef942afa355a72e7db0a59601bfb923ecaef
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-17 19:03:16 +0300

    init repository

---
+++ local/templates/bitrix-zero/description.php
@@ -0,0 +1,7 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$arTemplate = [
+	"NAME" => "Bitrix zero", 
+	"DESCRIPTION" => "Шаблон сайта Битрикс без лишних файлов", 
+];
+?>
---
+++ local/templates/bitrix-zero/header.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bitrix zero</title>
+  <?$APPLICATION->ShowHead();?>
+</head>
+<body>
+  <div id="panel"><?$APPLICATION->ShowPanel();?></div>
+  header
